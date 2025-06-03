3c9dbb0ccda18e9d0fb255215c35ae95c45918ff
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-06-03 14:49:19 +0300

    register User in Admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {'fields': (
+                'email', 'username', 'first_name',
+                'last_name', 'password1', 'password2', 'role'
+            )
+            }
+        ),
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Custom Fields', {
+                'fields': ('role',)
+                }
+            ),
+    )
+    list_display = (
+        'username',
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+        'role'
+    )
+    list_filter = ('email', 'username')
+    search_fields = ('email', 'username',)
+    empty_value_display = '-empty-'
