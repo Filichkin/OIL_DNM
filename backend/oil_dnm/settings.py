5eef4b439342799495e5a2a637e477531bc32379
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-06-11 15:36:10 +0300

    added SWAGGER_SETTINGS

--- backend/oil_dnm/settings.py
+++ backend/oil_dnm/settings.py
@@ -138,3 +138,23 @@ DJOSER = {
         'current_user': 'api.serializers.UserSerializer',
     }
 }
+
+SWAGGER_SETTINGS = {
+    'SHOW_REQUEST_HEADERS': True,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
+    'SUPPORTED_SUBMIT_METHODS': [
+        'get',
+        'post',
+        'put',
+        'delete',
+        'patch'
+    ],
+}
