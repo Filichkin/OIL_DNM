12c6c88917d427bb0e212fedf0287b2ac3d36d04
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-06-16 21:01:41 +0300

    Product register in admin

--- backend/catalog/admin.py
+++ backend/catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product
+
+
+@admin.register(Product)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'part_number',
+        'brand',
+        'package_count',
+        'volume',
+        'price',
+        'description',
+        'specification',
+        )
+    search_fields = ('name', 'part_number',)
