eb33bd0889a9b7ee292116a3447bae4b6b88c937
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-06-30 16:37:36 +0300

    upd generate_order_number

--- backend/api/orders/utils.py
+++ backend/api/orders/utils.py
@@ -1,5 +1,5 @@
 def generate_order_number(dealer, supplier: str):
     order_number = str(dealer) + supplier + str(0) * (
             6 - len(str(dealer.orders.count()))
-            ) + str(dealer.orders.count())
+            ) + str(dealer.orders.count() - 1)
     return order_number
